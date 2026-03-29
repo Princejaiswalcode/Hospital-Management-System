409be4cf35ada12ae121e0433a082f44542248a4
Mahek Yadav <mahekyadav325@gmail.com>
2026-03-29 12:17:30 +0530

    data inserted in appointments and table bills created

--- backend/src/db/schema.sql
+++ backend/src/db/schema.sql
@@ -43,3 +43,81 @@ CREATE TABLE `admissions` (
 
 --
 -- Dumping data for table `admissions`
+
+--
+
+LOCK TABLES `admissions` WRITE;
+/*!40000 ALTER TABLE `admissions` DISABLE KEYS */;
+INSERT INTO `admissions` VALUES (1,1,6,'2026-01-20',NULL,'ICU-01','B2','Cardiac monitoring'),(2,3,7,'2026-01-22','2026-01-25','GW-A-12','A12','Knee injury'),(3,5,8,'2026-01-23','2026-03-19','PR-05','P5','Observation'),(4,7,9,'2026-01-24','2026-01-26','GW-B-08','B8','Severe headache'),(5,9,10,'2026-01-25','2026-03-19','GW-A-09','A9','Skin infection'),(6,25,6,'2026-03-20','2026-03-19',NULL,NULL,NULL),(7,8,6,'2026-03-20','2026-03-19',NULL,NULL,NULL),(8,8,10,'2026-03-19',NULL,NULL,NULL,NULL),(11,1,6,'2026-03-19',NULL,NULL,NULL,NULL);
+/*!40000 ALTER TABLE `admissions` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `appointments`
+--
+
+DROP TABLE IF EXISTS `appointments`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `appointments` (
+  `appointment_id` int NOT NULL AUTO_INCREMENT,
+  `patient_id` int NOT NULL,
+  `doctor_id` int NOT NULL,
+  `appointment_date` date NOT NULL,
+  `appointment_time` time NOT NULL,
+  `status` varchar(20) DEFAULT 'Scheduled',
+  `reason` text,
+  `notes` text,
+  PRIMARY KEY (`appointment_id`),
+  KEY `fk_appointment_patient` (`patient_id`),
+  KEY `fk_appointment_doctor` (`doctor_id`),
+  CONSTRAINT `fk_appointment_doctor` FOREIGN KEY (`doctor_id`) REFERENCES `doctors` (`doctor_id`) ON DELETE CASCADE,
+  CONSTRAINT `fk_appointment_patient` FOREIGN KEY (`patient_id`) REFERENCES `patients` (`patient_id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=102 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `appointments`
+--
+
+LOCK TABLES `appointments` WRITE;
+/*!40000 ALTER TABLE `appointments` DISABLE KEYS */;
+INSERT INTO `appointments` VALUES (11,1,61,'2026-01-25','10:30:00','completed','Chest pain','Urgent checkup'),(13,3,63,'2026-01-26','09:00:00','Scheduled','Knee pain',NULL),(14,4,64,'2026-01-26','14:30:00','Cancelled','Skin allergy','Patient unavailable'),(15,5,65,'2026-01-27','16:00:00','Scheduled','Routine checkup',NULL),(16,6,61,'2026-01-27','12:00:00','Completed','Heart follow-up','Stable condition'),(17,7,62,'2026-01-28','10:45:00','Scheduled','Headache',NULL),(18,8,63,'2026-01-28','15:30:00','Scheduled','Back pain',NULL),(19,9,64,'2026-01-29','09:30:00','Completed','Rash','Cream prescribed'),(20,10,65,'2026-01-29','11:00:00','Scheduled','Pregnancy consultation',NULL),(21,3,62,'2026-02-03','11:15:00','Scheduled','Regular checkup','First visit'),(22,23,67,'2026-02-06','10:30:00','Scheduled','Fever','urgent'),(23,22,64,'2026-02-07','15:00:00','Scheduled','Fever','urgent'),(25,25,64,'2026-02-12','15:00:00','Scheduled','aids','urgent'),(26,22,66,'2026-03-19','10:30:00','Scheduled','23','23'),(27,22,70,'2026-03-20','11:30:00','Scheduled','fever',NULL),(28,22,61,'2026-03-20','10:30:00','Scheduled','fever',NULL),(29,22,62,'2026-03-20','10:30:00','Scheduled','fever',NULL),(30,23,69,'2026-03-21','11:30:00','Scheduled','232323',NULL),(31,3,69,'2026-03-20','09:30:00','Scheduled','23222',NULL),(32,1,69,'2026-03-19','11:30:00','Scheduled','fever',NULL),(101,5,61,'2026-04-01','10:00:00','Scheduled',NULL,NULL);
+/*!40000 ALTER TABLE `appointments` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `bills`
+--
+
+DROP TABLE IF EXISTS `bills`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `bills` (
+  `bill_id` int NOT NULL AUTO_INCREMENT,
+  `patient_id` int NOT NULL,
+  `appointment_id` int DEFAULT NULL,
+  `treatment_id` int DEFAULT NULL,
+  `admission_id` int DEFAULT NULL,
+  `consultation_charge` decimal(10,2) DEFAULT '0.00',
+  `medicine_charge` decimal(10,2) DEFAULT '0.00',
+  `room_charge` decimal(10,2) DEFAULT '0.00',
+  `total_amount` decimal(10,2) NOT NULL,
+  `payment_status` varchar(20) NOT NULL,
+  `bill_date` date NOT NULL,
+  `payment_date` date DEFAULT NULL,
+  PRIMARY KEY (`bill_id`),
+  KEY `fk_bill_patient` (`patient_id`),
+  KEY `fk_bill_appointment` (`appointment_id`),
+  KEY `fk_bill_treatment` (`treatment_id`),
+  KEY `fk_bill_admission` (`admission_id`),
+  CONSTRAINT `fk_bill_admission` FOREIGN KEY (`admission_id`) REFERENCES `admissions` (`admission_id`) ON DELETE SET NULL,
+  CONSTRAINT `fk_bill_appointment` FOREIGN KEY (`appointment_id`) REFERENCES `appointments` (`appointment_id`) ON DELETE SET NULL,
+  CONSTRAINT `fk_bill_patient` FOREIGN KEY (`patient_id`) REFERENCES `patients` (`patient_id`) ON DELETE CASCADE,
+  CONSTRAINT `fk_bill_treatment` FOREIGN KEY (`treatment_id`) REFERENCES `treatments` (`treatment_id`) ON DELETE SET NULL
+) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `bills`
+--
